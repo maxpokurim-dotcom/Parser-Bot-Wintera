8fd45fb41a8f44e69119da8560a70ff65d9025be
maxpokurim-dotcom <maxpokurim@gmail.com>
2025-12-01 15:41:36 +0300

    Create __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,8 @@
+"""
+Core module initialization
+Telegram Bot for Parsing, Mailing and Automation
+Version 3.0 with Herder (Botovod) module
+"""
+
+__version__ = '3.0.0'
+__author__ = 'Bot Developer'
